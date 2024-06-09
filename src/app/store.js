fe27fc230627cd5358b3bba742800dcca6652405
Yaman Aswal <yaman@appsquad.com>
2024-06-09 10:54:15 +0530

    Code with redux toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -1,19 +1,13 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "../features/accounts/slices/AccountSlice";
 import customerReducer from "../features/customers/slices/CustomerSlice";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension/lib/types/logOnly";
 
-
-const rootReducer = combineReducers({
-    account: accountReducer,
-    customer: customerReducer,
+const store = configureStore({
+    reducer: {
+        account: accountReducer,
+        customer: customerReducer
+    }
 })
-
-
-//Step 3: Pass reducer function to redux store to manage it by redux
-// Also added redux dev tools
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 
 export default store;
